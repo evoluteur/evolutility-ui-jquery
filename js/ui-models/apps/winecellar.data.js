faf165de2e22e1189a8c14469463b3ebe760c636
Olivier Giulieri <olivier@evolutility.org>
2014-01-25 23:32:02 -0800

    Better sample data for wine cellar app.

---
+++ js/ui-models/apps/winecellar.data.js
@@ -0,0 +1,96 @@
+
+var winecellar_data = [
+    {
+        name: 'Macrostie',
+        label_img: 'pix/wine/macrostie.gif',
+        vintage: 2010,
+        winery:'Mc Williams',
+        bsize: '750',
+        type: 'red',
+        price: 20
+    },
+    {
+        name: "Château d'Yquem",
+        label_img: 'pix/wine/yquem.gif',
+        vintage: 2012,
+        winery:'Lur Saluces',
+        bsize: '750',
+        type: 'white',
+        grape: "cb",
+        price: 240,
+        value: 270,
+        purchased: 3,
+        remaining: 1,
+        degustation: [
+            {
+                ddate: '2009-4-23',
+                robe: 'golden',
+                nose: 'fruty',
+                taste: 'incredible'
+            },
+            {
+                ddate: '2012-24-12',
+                robe: 'clear gold',
+                nose: 'strong and sweet',
+                taste: 'outstanding'
+            }
+        ]
+    },
+    {
+        name: 'Château St Jean',
+        label_img: 'pix/wine/stjean.gif',
+        vintage: 2008,
+        winery:'Ch St Jean',
+        bsize: '750',
+        type: 'red',
+        price: 34,
+        degustation: [
+            {
+                ddate: '2013-24-12',
+                robe: 'thick',
+                nose: 'strong',
+                taste: 'good',
+                notes:'Great w/ beef.'
+            }
+        ]
+    },
+    {
+        name: 'Vine Cliff',
+        label_img: 'pix/wine/vinecliff.gif',
+        vintage: 2013,
+        winery:'Vine Cliff',
+        bsize: '750',
+        type: 'red',
+        price: 18,
+        degustation: [
+            {
+                ddate: '2012-5-5',
+                robe: 'light',
+                nose: 'strong',
+                taste: 'good'
+            },
+            {
+                ddate: '2012-8-10',
+                robe: 'light',
+                nose: 'strong',
+                taste: 'good'
+            },
+            {
+                ddate: '2013-8-18',
+                robe: 'light',
+                nose: 'strong',
+                taste: 'ok'
+            }
+        ]
+    },
+    {
+        name: 'Château Montelena',
+        label_img: 'pix/wine/montelena.gif',
+        vintage: 2005,
+        winery:'Château Montelena',
+        bsize: '1500',
+        type: 'red',
+        region:'California',
+        price: 62
+    }
+];
