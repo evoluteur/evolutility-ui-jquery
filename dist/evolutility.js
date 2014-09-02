276148b145a552e529ab3223ccbcd8f1dff635ba
Olivier Giulieri <olivier.giulieri@illum.io>
2014-09-01 18:30:34 -0700

    Version 0.3.1

--- dist/evolutility.js
+++ dist/evolutility.js
@@ -1,4 +1,4 @@
-/*   evolutility v0.3   */
+/*   evolutility v0.3.1   */
 /*   (c) 2014 Olivier Giulieri   */
 /*   https://github.com/evoluteur/evolutility   */
 /*! ***************************************************************************
