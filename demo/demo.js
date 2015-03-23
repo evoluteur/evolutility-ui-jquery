c885d8c426f5a37c3952d2c479c119b94fbe6662
Olivier Giulieri <olivier@cyence.net>
2015-03-22 23:42:54 -0700

    Text edits.

--- demo/demo.js
+++ demo/demo.js
@@ -9,8 +9,19 @@
  *
  *************************************************************************** */
 
-//TODO consolidate this code
 var uidef=null;
+
+var ViewDescriptions = {
+ 'view': {name: 'View', desc: 'The "View" view shows all fields for viewing (read only). Fields are grouped in panels and tabs.'},
+ 'edit': {name: 'Edit', desc: 'The "Edit" view shows all fields for edition to create or update models. It automatically performs validation based on the UI-model and supports the Master-Details pattern (nested collections). Fields are grouped in panels and tabs.'},
+ 'mini': {name: 'Mini (quick-edit)', desc: 'The "Mini" (quick-edit) view only shows important fields (required or showing as a column in grids). Fields are grouped in a single panel.'},
+ 'json': {name: 'JSON', desc: 'The "JSON" view shows the JSON representation of the data.'},
+ 'list': {name: 'List', desc: 'The "List" view gives a tabular view of a collection with paging.'},
+ 'cards': {name: 'Cards', desc: 'The "Cards" Shows records side by side as cards.'},
+ 'charts': {name: 'Charts', desc: 'The "Charts" view draws charts about the collection.'},
+ 'filter': {name: 'Filter', desc: 'The "Filter" view is used to build a structured query to filter a collection.'},
+ 'export': {name: 'Export', desc: 'The "Export" view let\'s you define export options and preview the collection export in different data formats (CSV, TAB, HTML, XML, SQL and JSON).'}
+};
 
 function createSampleDataIfEmpty(entityName){
     var lc = new Backbone.LocalStorage('evol-'+entityName),
